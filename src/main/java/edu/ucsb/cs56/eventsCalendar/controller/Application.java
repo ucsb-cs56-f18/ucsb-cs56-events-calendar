cb31e8b03c675b91115ab97353d383dd4b13742d
Jeffrey Tellew <jtellew@umail.ucsb.edu>
2018-12-06 17:28:37 -0800

    Removed DS_store and fixed some weird stuff from the Scraping branch

--- src/main/java/edu/ucsb/cs56/eventsCalendar/controller/Application.java
+++ src/main/java/edu/ucsb/cs56/eventsCalendar/controller/Application.java
@@ -7,14 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 @SpringBootApplication
 public class Application implements CommandLineRunner{
     public static void main(String[] args) {
-<<<<<<< HEAD:src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/Application.java
       SpringApplication.run(Application.class, args);
-=======
-	  SpringApplication.run(Application.class, args);
->>>>>>> b6f9ee90b61b922258d1ef39033c752ae445c8b8:src/main/java/edu/ucsb/cs56/eventsCalendar/controller/Application.java
     }
+    
     @Override
-public void run(String... args) throws Exception {
-  main.java.edu.ucsb.cs56.events_calendar.Scraping.main(new String[0]);
-  }
+    public void run(String... args) throws Exception {
+        main.java.edu.ucsb.cs56.events_calendar.Scraping.main(new String[0]);
+    }
 }
--- src/main/java/edu/ucsb/cs56/events_calendar/Event.java
+++
@@ -1,3 +0,0 @@
-package main.java.edu.ucsb.cs56.events_calendar;
-
-public class Event{}
